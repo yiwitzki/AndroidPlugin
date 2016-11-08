2b851a6ca263a0a6528025217fd35d5300f4d555
tangpeng <tangpeng02@baidu.com>
2016-11-08 16:02:15 +0800

    first commit

---
+++ app/src/main/java/com/tangpeng/androidplugin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tangpeng.androidplugin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
